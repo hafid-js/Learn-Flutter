19756503e77122de722aaaa617b43d690109c51f
hafidjs <afidzpratama@gmail.com>
2025-09-13 07:00:25 +0700

    text weight

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.teal,
         appBar: AppBar(title: Text("MyApps"),),
 
         body: Center(
-          child: Text("HALO"),
+          child: Text(
+            "Lorem Ipsum is simply dummy text of the printing andindustry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.right,
+            style: TextStyle(
+              backgroundColor: Colors.blue,
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 5,
+              fontFamily: 'Stick',
+              decorationStyle: TextDecorationStyle.wavy,
+              decoration: TextDecoration.lineThrough,
+              decorationColor: Colors.pink,
+              decorationThickness: 5
+            ),
+          ),
         ),
               ),
     );
