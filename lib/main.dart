d24be6d33231bb8f138f22538c62cc71454d15e7
hafidjs <afidzpratama@gmail.com>
2025-09-15 14:43:38 +0700

    stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:faker/faker.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-   var faker = Faker();
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 1;
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+     if(counter != 1) {
+       counter--;
+     }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Extract Widget"),
+          title: const Text("Dynamic Apps"),
         ),
-        body: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context,index) {
-            return ChatItem(imageUrl: "https://picsum.photos/id/$index/200/300", title: faker.person.name(), subtitle: faker.lorem.sentence());
-          }
-        )
-      )
-    );
-  }
-}
-
-class ChatItem extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String subtitle;
-  
- const ChatItem({
-    Key? key,
-    required this.imageUrl,
-    required this.title,
-    required this.subtitle,
-  }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(imageUrl),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              counter.toString(),
+              style: TextStyle(fontSize: 20 + double.parse(counter.toString())),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: increment,
+                  child: const Icon(Icons.add),
+                ),
+                ElevatedButton(
+                  onPressed: decrement,
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      title: Text(title),
-      subtitle: Text(subtitle),
-      trailing: Text("10:00 WIB"),
     );
   }
 }
