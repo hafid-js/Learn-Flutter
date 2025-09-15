8aefe9cba876712056d3b1fabaee9b97f3533395
hafidjs <afidzpratama@gmail.com>
2025-09-15 12:46:36 +0700

    list tile

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,70 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  List<Color> myColor = [Colors.red, Colors.green, Colors.blue, Colors.amber];
-  final List<Widget> myList = List.generate(
-    100,
-    (index) => Text(
-      "${index + 1}",
-      style: TextStyle(fontSize: 20 + double.parse(index.toString())),
-    ),
-  );
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("List View")),
-        body: ListView(children: myList),
+        appBar: AppBar(
+          title: Text("List Tile"),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+              title: Text("Nadia Rahayu"),
+              subtitle: Text("is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", maxLines: 2, overflow: TextOverflow.ellipsis,),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 WIB"),
+              // onTap: () {
+              //   return
+              // },
+              tileColor: Colors.pink,
+              dense: true,
+            ),
+           Divider(
+              color: Colors.black
+            ),
+            ListTile(
+              title: Text("Nadia Rahayu"),
+              subtitle: Text("This is subtitle okay..."),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 WIB"),
+            ),
+            Divider(
+              color: Colors.black
+            ),
+            ListTile(
+              title: Text("Nadia Rahayu"),
+              subtitle: Text("This is subtitle okay..."),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 WIB"),
+            ),
+            Divider(
+              color: Colors.black
+            ),
+            ListTile(
+              title: Text("Nadia Rahayu"),
+              subtitle: Text("This is subtitle okay..."),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 WIB"),
+            ),
+            Divider(
+              color: Colors.black
+            ),
+            ListTile(
+              title: Text("Nadia Rahayu"),
+              subtitle: Text("This is subtitle okay..."),
+              leading: CircleAvatar(),
+              trailing: Text("10:00 WIB"),
+            ),
+            Divider(
+              color: Colors.black
+            ),
+          ],
+        ),
       ),
     );
   }
