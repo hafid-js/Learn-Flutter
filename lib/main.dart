3eb6868b118e2b2dec00298c694246edb1d6c80a
hafidjs <afidzpratama@gmail.com>
2025-09-15 14:03:45 +0700

    extract widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:faker/faker.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+   var faker = Faker();
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Image Widget"),
+          title: Text("Extract Widget"),
         ),
-        body: Center(
-          child: Container(
-            width: 350,
-            height: 500,
-            color: Colors.pink,
-            child: Image.asset("images/image.jpg", fit: BoxFit.cover),
-            // child: Image(
-            //   fit: BoxFit.cover,
-            //   image: AssetImage("images/image.jpg")
-            //   // image: NetworkImage("https://picsum.photos/seed/picsum/350/500")
-            //   ),
-          ),
-        ),
+        body: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (context,index) {
+            return ChatItem(imageUrl: "https://picsum.photos/id/$index/200/300", title: faker.person.name(), subtitle: faker.lorem.sentence());
+          }
+        )
+      )
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+  
+ const ChatItem({
+    Key? key,
+    required this.imageUrl,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
       ),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: Text("10:00 WIB"),
     );
   }
 }
