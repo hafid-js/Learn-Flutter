cbd710bec4491af987397b013129f5853c5f20b4
hafidjs <afidzpratama@gmail.com>
2025-09-15 15:23:19 +0700

    Mapping List

--- lib/main.dart
+++ lib/main.dart
@@ -4,56 +4,91 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int counter = 1;
-
-  void increment() {
-    setState(() {
-      counter++;
-    });
-  }
-
-  void decrement() {
-    setState(() {
-     if(counter != 1) {
-       counter--;
-     }
-    });
-  }
+class MyApp extends StatelessWidget {
+  final List<Map<String, dynamic>> myList = [
+    {
+      "Name": "Nadia",
+      "Age": 28,
+      "favColor": [
+        "Black",
+        "Red",
+        "Amber",
+        "Black",
+        "Red",
+        "Amber",
+        "Black",
+        "Red",
+        "Amber",
+        "Black",
+        "Red",
+        "Amber",
+      ],
+    },
+    {
+      "Name": "Hafid",
+      "Age": 27,
+      "favColor": ["Red", "White", "Green"],
+    },
+    {
+      "Name": "Aulia",
+      "Age": 26,
+      "favColor": ["Pink", "Yellow", "Green"],
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Dynamic Apps"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              counter.toString(),
-              style: TextStyle(fontSize: 20 + double.parse(counter.toString())),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: increment,
-                  child: const Icon(Icons.add),
+        appBar: AppBar(title: Text("ID Apps")),
+        body: ListView(
+          children: myList.map((data) {
+            List myFavColor = data["favColor"];
+            return Card(
+              margin: EdgeInsets.all(20),
+              color: Colors.black.withOpacity(0.1),
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name : ${data['Name']}"),
+                            Text("Age :  ${data['Age']}"),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: myFavColor.map((color) {
+                          return Container(
+                            color: Colors.amber,
+                            margin: EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 8,
+                            ),
+                            padding: EdgeInsets.all(10),
+                            child: Text(color),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: decrement,
-                  child: const Icon(Icons.remove),
-                ),
-              ],
-            ),
-          ],
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
