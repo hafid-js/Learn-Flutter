4f989331d8241207af1a6ed6dc2e9a0fba61ea4a
hafidjs <afidzpratama@gmail.com>
2025-09-16 17:26:27 +0700

    text field

--- lib/main.dart
+++ lib/main.dart
@@ -5,63 +5,24 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  List<Tab> myTab = [
-    Tab(icon: Icon(Icons.add_a_photo)),
-    Tab(icon: Icon(Icons.ac_unit_outlined)),
-    Tab(text: "Tab 3"),
-  ];
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        initialIndex: 2,
-        length: myTab.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("My Apps"),
-              bottom: TabBar(
-                labelColor: Colors.black,
-                unselectedLabelColor: Colors.green,
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  
-                ),
-                unselectedLabelStyle: TextStyle(
-                  fontWeight: FontWeight.normal
-                ),
-                // indicatorColor: Colors.black,
-                // indicatorWeight: 5,
-                // indicatorPadding: EdgeInsets.all(10),
-                indicator: BoxDecoration(
-                  color: Colors.amber,
-                  
-                  
-                  // borderRadius: BorderRadius.circular(50),
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.black,
-                      width: 5
-                    )
-                  )
-                ),
-                tabs: myTab,
-                ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Fitur Text Field")),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextField(autocorrect: false, autofocus: false, enableSuggestions: true, enableInteractiveSelection: false, keyboardType: TextInputType.phone ),
+              ],
             ),
-            body: TabBarView(
-              children:[
-              Center(
-                child: Text("Tab 1"),
-              ),
-              Center(
-                child: Text("Tab 2"),
-              ),
-              Center(
-                child: Text("Tab 3"),
-              ),
-              ]),
           ),
         ),
+      ),
     );
   }
 }
