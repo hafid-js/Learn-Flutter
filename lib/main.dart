178a57935889d53991b54d7838931f1272a7b2c1
hafidjs <afidzpratama@gmail.com>
2025-09-17 06:28:41 +0700

    using text field

--- lib/main.dart
+++ lib/main.dart
@@ -4,85 +4,49 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController myController = TextEditingController();
+
+  String hasil = "HASIL INPUT";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Fitur Text Field")),
+        appBar: AppBar(title: Text("Text Field")),
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: 
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
                 TextField(
-                  showCursor: true,
-                  autocorrect: false,
-                  cursorColor: Colors.amber,
-                  // cursorWidth: 10,
-                  // cursorHeight: 25,
-                  // cursorRadius: Radius.circular(20),
+                  controller: myController,
+                  onChanged: (value) {
+                    print("ONCHANGE");
 
-                  textAlign: TextAlign.start,
-                  textAlignVertical: TextAlignVertical.bottom,
-                  textCapitalization: TextCapitalization.words,
-
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 20
-                  ),
-
-                  decoration: InputDecoration(
-
-                    icon: Icon(
-                      Icons.person,
-                      size: 35,
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.red
-                      )
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color:Colors.black
-                      )
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.red
-                      )
-                    ),
-
-                    prefixIcon: Icon(Icons.add_a_photo),
-                    suffixIcon: IconButton(onPressed: () {
-                      
-                    }, icon: Icon(Icons.remove_red_eye))
-                    // prefixIcon: Icon(
-                    //   Icons.person_add,
-                    //   size: 35,
-                    // ),
-                    // prefixText: "Name: ",
-                    // prefix: Icon(
-                    //   Icons.person_add,
-                    //   size: 35,
-                    // )
-
-                    hintText: "Please input your name..",
-                    hintStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20
-                  ),
-                    labelText: "Full Name",
-                    labelStyle: TextStyle(
-                      color: Colors.black
-                    )
-                  ),
-                )
+                   setState(() {
+                      hasil = value;
+                   });
+                  },
+                  onSubmitted: (value) {
+                    print(value);
+                  },
+                  onEditingComplete: () {
+                    print("EDIT SUCCESS");
+                  },
+                ),
+                Text(hasil),
+              ],
             ),
           ),
         ),
-      );
+      ),
+    );
   }
 }
