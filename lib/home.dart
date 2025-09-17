1d6df0b23e78c2b446e88e2692cf055916f8d4ed
hafidjs <afidzpratama@gmail.com>
2025-09-17 10:02:30 +0700

    dismissible

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
+  final Faker faker = Faker();
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -11,45 +13,57 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Dialog")),
-      body: Center(
-        child: Text(data, style: TextStyle(fontSize: 35),),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Text("Confirm"),
-                content: Text("Are u sure to delete this content?"),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop(false);
-                      setState(() {
-                        data = "False";
-                      });
-                    },
-                    child: Text("No"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop(true);
-                      setState(() {
-                        data = "True";
-                      });
-                    },
-                    child: Text("Yes"),
-                  ),
-                ],
-              );
+      appBar: AppBar(title: Text("Dismissible")),
+      body: ListView.builder(
+        padding: EdgeInsets.all(8),
+        itemCount: 100,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            onDismissed: (direction) {
+              print("Dismissed");
+              print(direction);
+              if(direction == DismissDirection.endToStart) {
+                print("End to Start");
+              } else {
+                print("Start to end");
+              }
             },
-          ).then((value) => print(value));
+            confirmDismiss: (direction) {
+              return showDialog(context: context, builder:(context) {
+                return AlertDialog(
+                  title: Text("Confirm"),
+                  content: Text("Are you sure to delete this item ?"),
+                  actions: [
+                    TextButton(onPressed: () {
+                     Navigator.of(context).pop(false);
+                    }, child: Text("No")),
+                    TextButton(onPressed: () {
+                      Navigator.of(context).pop(true);
+                    }, child: Text("Yes"))
+                  ],
+                );
+              },);
+            },
+            key: Key(index.toString()),
+            // direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.red,
+              child: Icon(Icons.delete, size: 25,),
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.only(right: 10),
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Center(
+                  child: Text("${index + 1}"),
+                ),
+              ),
+              title: Text(faker.person.name()),
+              subtitle: Text(faker.lorem.sentence()),
+            ),
+          );
         },
-        child: Icon(Icons.delete),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
