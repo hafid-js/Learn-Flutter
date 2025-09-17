07e79fad20a99bbc88af9d265247dc0c53cb66d2
hafidjs <afidzpratama@gmail.com>
2025-09-17 17:27:50 +0700

    Theme

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,66 +1,18 @@
-import 'dart:math';
-import 'package:learn_flutter_1/models/product.dart';
 import 'package:flutter/material.dart';
-import 'package:faker/faker.dart' as faker;
 
 void main() {
-  runApp(HomePage());
+  runApp(MyHomePage())
 }
 
-class HomePage extends StatelessWidget {
-  final faker.Faker fake = faker.Faker();
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<Product> dummyData = List.generate(100, (index) {
-      return Product(
-        "https://picsum.photos/id/$index/200",
-        fake.food.restaurant(),
-        10000 + Random().nextInt(100000),
-        fake.lorem.sentence(),
-      );
-    });
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Marketplace")),
-        body: GridView.builder(
-          padding: EdgeInsets.all(10),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-          ),
-          itemBuilder: (context, index) {
-            return GridTile(
-              child: Image.network(dummyData[index].imageURL),
-              footer: Container(
-                height: 80,
-                color: Colors.green.withOpacity(0.8),
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      dummyData[index].judul,
-                      maxLines: 1,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text("Rp. ${dummyData[index].harga}"),
-                    Text(
-                      dummyData[index].deskripsi,
-                      maxLines: 2,
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-          itemCount: dummyData.length,
+        appBar: AppBar(
+          title: Text("Home Page"),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter_1/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +10,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        // visualDensity: VisualDensity.adaptivePlatformDensity,
+        // visualDensity: VisualDensity.comfortable,
+        // visualDensity: VisualDensity.compact,
+        // visualDensity: VisualDensity(horizontal: 4, vertical: 0),
+        // primaryColor: Colors.red
+        // primarySwatch: MaterialColor(0xFFF5E0CE, <int, Color>{
+        //   50: Color(0x1a5D424),
+        //   100: Color(0xa15D4524),
+        //   // dan seterusnya, bebas custom
+        // }),
+        // appBarTheme: AppBarTheme()
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Theme App")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("This is a text.", style: TextStyle(fontSize: 35)),
+              SizedBox(height: 20),
+              Text("This is a text.", style: TextStyle(fontSize: 35)),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Button", style: TextStyle(fontSize: 35)),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.mediation),
+        ),
+      ),
     );
   }
 }
