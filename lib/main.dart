de08c40c428b94cc9c318e85b395481b9976e0ce
hafidjs <afidzpratama@gmail.com>
2025-09-28 16:33:31 +0700

    dropdown search

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,61 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import './providers/colors.dart';
-import './pages/home_page.dart';
-import './pages/add_color_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MultiColor(),
-      builder: (context, child) => MaterialApp(
-        home: HomePage(),
-        routes: {
-          HomePage.routeName: (ctx) => HomePage(),
-          AddColorPage.routeName: (ctx) => AddColorPage(),
-        },
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> data = ["hafid", "aulia", "eni"];
+
+  String? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Dropdown')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: DropdownSearch<String>(
+        items: (f, cs) => data,
+        popupProps: PopupProps.menu(
+          showSelectedItems: true,
+          
+          disabledItemFn: (String? data) => data?.startsWith('h') ?? false,
+          fit: FlexFit.loose,
+          
+          
+        ),
+        decoratorProps: DropDownDecoratorProps(
+          decoration: InputDecoration(
+            labelText: "Menu Mode",
+            hintText: "Select Human ...",
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
+            contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            )
+          
+          ),
+        ),
+        
       ),
-    );
+        ),
+      );
   }
 }
