bc1654e3595dfc2d6224de3205bf2c398764341a
hafidjs <afidzpratama@gmail.com>
2025-09-28 15:30:36 +0700

    checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter_1/providers/all_products.dart';
-import 'package:learn_flutter_1/providers/cart.dart';
-import 'package:learn_flutter_1/screens/cart_screen.dart';
-import 'package:learn_flutter_1/screens/product_detail_screen.dart';
-import 'package:learn_flutter_1/screens/products_overview_screen.dart';
 import 'package:provider/provider.dart';
 
+import './providers/colors.dart';
+import './pages/home_page.dart';
+import './pages/add_color_page.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => Cart()),
-        ChangeNotifierProvider(create: (context) => Products()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'MyShop',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.indigo,
-          ).copyWith(secondary: Colors.amber),
-        ),
-        home: ProductsOverviewScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => MultiColor(),
+      builder: (context, child) => MaterialApp(
+        home: HomePage(),
         routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          CartScreen.routeName: (ctx) => CartScreen(),
+          HomePage.routeName: (ctx) => HomePage(),
+          AddColorPage.routeName: (ctx) => AddColorPage(),
         },
       ),
     );
   }
 }
-
-
-// class ScreenSementara extends StatelessWidget {
-//   List<Text> myList = List.generate(50, (index) => Text("${index + 1}", style: TextStyle(fontSize: 35),));
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(),
-//       body: ListView(
-//         children: 
-//           [...myList,
-//           Container(width: 100, height: 100, color: Colors.green,)],
-//       ),
-//     );
-//   }
-// }
