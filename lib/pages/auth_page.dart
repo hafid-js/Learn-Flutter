dc1f20b46fe6af0ec13b86531fe672a4b2bb4b83
hafidjs <afidzpratama@gmail.com>
2025-09-30 15:39:31 +0700

    authentication - login and register

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'package:learn_flutter_1/pages/home_page.dart';
+import 'package:learn_flutter_1/providers/auth.dart' as myAuth;
+import 'package:provider/provider.dart';
 
-const users =  {
+const users = {
   'dribbble@gmail.com': '12345',
   'hunter@gmail.com': 'hunter',
 };
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   Duration get loginTime => const Duration(milliseconds: 2250);
 
-  Future<String?> _authUser(LoginData data) {
-    debugPrint('Name: ${data.name}, Password: ${data.password}');
+  Future<String?> _authUser(LoginData data) async {
+    debugPrint('Login -> Name: ${data.name}, Password: ${data.password}');
+
     return Future.delayed(loginTime).then((_) {
       if (!users.containsKey(data.name)) {
-        return 'User not exists';
+        return 'User does not exist';
       }
       if (users[data.name] != data.password) {
         return 'Password does not match';
@@ -25,18 +33,47 @@ class LoginPage extends StatelessWidget {
     });
   }
 
-  Future<String?> _signupUser(SignupData data) {
-    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      return null;
+  Future<String?> _authUserSignUp(SignupData data) async {
+    debugPrint('Signup -> Name: ${data.name}, Password: ${data.password}');
+
+    final name = data.name ?? '';
+    final password = data.password ?? '';
+
+    return Future.delayed(loginTime).then((_) async {
+      try {
+        await Provider.of<myAuth.Auth>(context, listen: false).signup(name, password);
+        return null; 
+      } catch (error) {
+        debugPrint('Signup error: $error');
+        return 'Signup failed: $error';
+      }
     });
   }
 
-  Future<String?> _recoverPassword(String name) {
-    debugPrint('Name: $name');
+  Future<String?> _authUserLogin(LoginData data) async {
+    debugPrint('Login -> Name: ${data.name}, Password: ${data.password}');
+
+    final name = data.name ?? '';
+    final password = data.password ?? '';
+
+    return Future.delayed(loginTime).then((_) async {
+      try {
+        await Provider.of<myAuth.Auth>(context, listen: false).login(name, password);
+        return null; 
+      } catch (error) {
+        debugPrint('Login error: $error');
+        return 'Login failed: $error';
+      }
+    });
+  }
+
+
+  Future<String?> _recoverPassword(String name) async {
+    debugPrint('Recover -> Name: $name');
+
     return Future.delayed(loginTime).then((_) {
       if (!users.containsKey(name)) {
-        return 'User not exists';
+        return 'User does not exist';
       }
       return null;
     });
@@ -46,15 +83,14 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return FlutterLogin(
       title: 'ECORP',
-      // logo: const AssetImage('assets/images/ecorp-lightblue.png'),
-      onLogin: _authUser,
-      onSignup: _signupUser,
-      onSubmitAnimationCompleted: () {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (context) => HomePage(),
-        ));
-      },
+      onLogin: _authUserLogin,
+      onSignup: _authUserSignUp,
       onRecoverPassword: _recoverPassword,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      },
     );
   }
 }
---
+++ lib/providers/auth.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  
+
+  signup(String email, String password) async {
+    Uri url = Uri.parse(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBLO1OZ-Lil489qehnsTrguRm_jaCvaaQE");
+    try {
+      var response = await http.post(
+        url,
+        body: json.encode({
+          "email": email,
+          "password": password,
+          "returnSecureToken": true,
+        }),
+      );
+
+      print(json.decode(response.body));
+    } catch (err) {
+      throw (err);
+    }
+  }
+
+
+  login(String email, String password) async {
+    Uri url = Uri.parse(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBLO1OZ-Lil489qehnsTrguRm_jaCvaaQE");
+    try {
+      var response = await http.post(
+        url,
+        body: json.encode({
+          "email": email,
+          "password": password,
+          "returnSecureToken": true,
+        }),
+      );
+
+      print(json.decode(response.body));
+    } catch (err) {
+      throw (err);
+    }
+  }
+}
