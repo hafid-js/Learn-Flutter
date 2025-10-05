37589cd262955231992aef25ac43fa210f1dac20
hafidjs <afidzpratama@gmail.com>
2025-10-05 22:03:42 +0700

    bloc listener

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:learn_flutter_1/counter.dart';
-import 'package:learn_flutter_1/theme.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDark = false;
+
   final ThemeData dark = ThemeData.dark();
+
   final ThemeData light = ThemeData.light();
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-      BlocProvider(create: (context) => ThemeBloc(),
-    ),
-    BlocProvider(create: (context) => CounterBloc(),)
-    ],
-    child: BlocProvider(
-        create: (context) => ThemeBloc(),
-        child: BlocBuilder<ThemeBloc, bool>(builder: (context, state) => MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: state? dark : light,
-          home: HomePage(),
-        ),) 
-      ),
+        BlocProvider(create: (context) => CounterCubit()),
+        BlocProvider(create: (context) => ThemeBloc()),
+      ],
+      child: MultiBlocListener(listeners: [
+        BlocListener<CounterCubit, int>(listener: (context, state) {
+          if (state >= 10) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("Lebih dari 10"),
+                  duration: Duration(milliseconds: 500),
+                ),
+              );
+            }
+            if(state % 1 == 0) {
+              setState(() {
+                isDark = !isDark;
+              });
+            }
+        }),
+      ],
+      child: BlocBuilder<ThemeBloc, bool>(builder: (context, state) => MaterialApp(
+        theme: state == isDark ? dark : light,
+        home: HomePage()),
+      )
+    )
     );
   }
 }
@@ -37,24 +56,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Counter Apps")),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () => context.read<ThemeBloc>().changeTheme(), icon: Icon(Icons.refresh))
+        ],
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<CounterBloc, int>(builder: (context, state) => Text("Angka saat ini : $state", style: TextStyle(fontSize: 25)),),
-            SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(onPressed: () => context.read<CounterBloc>().decrement(), icon: Icon(Icons.remove)),
-                IconButton(onPressed: () => context.read<CounterBloc>().increment(), icon: Icon(Icons.add)),
-              ],
-            ),
-          ],
+        child: BlocListener<CounterCubit, int>(
+          listener: (context, state) {
+            
+          },
+          child: BlocBuilder<CounterCubit, int>(
+            builder: (context, state) =>
+                Text("$state", style: TextStyle(fontSize: 25)),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () => context.read<ThemeBloc>().changeTheme(), child: Icon(Icons.color_lens),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<CounterCubit>().increment(),
+      ),
     );
   }
+}
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+}
+
+class ThemeBloc extends Cubit<bool> {
+  ThemeBloc() : super(false);
+
+  void changeTheme() => emit(!state);
 }
--- lib/theme.dart
+++
@@ -1,9 +0,0 @@
-
-
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class ThemeBloc extends Cubit<bool> {
-  ThemeBloc() : super(false);
-
-  void changeTheme() => emit(!state);
-}
