f515d47fb49bec0ebd5630692df5136d3d3e0bb9
hafidjs <afidzpratama@gmail.com>
2025-10-05 10:16:31 +0700

    bloc and cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,152 +1,49 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learn_flutter_1/blocs/counter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int counter = 0;
-  bool isDark = false;
-
-  Future<void> setPreference() async {
-    final jembatan = await SharedPreferences.getInstance();
-
-    if(jembatan.containsKey('myData')){
-      jembatan.clear();
-    }
-
-    final myData = json.encode({
-      'counter': counter.toString(),
-      'isDark': isDark.toString(),
-    });
-
-    jembatan.setString('myData', myData);
-    setState(() {});
-  }
-
-
-  Future<void> getPreference() async {
-    final jembatan = await SharedPreferences.getInstance();
-
-    if(jembatan.containsKey('myData')){
-      final myData = json.decode(jembatan.getString('myData')!) as Map<String, dynamic>;
-
-      counter = int.parse(myData["counter"]);
-      isDark = myData["isDark"] == "true" ? true : false;
-    }
-  }
-
-  void minus() {
-    counter = counter - 1;
-    setPreference();
-  }
-
-  void plus() {
-    counter = counter + 1;
-    setPreference();
-  }
-
-void changeTheme() {
-  setState(() {
-    isDark = !isDark;
-    setPreference();
-  });
-}
-
-void refresh() {
-  counter = 0;
-  isDark = false;
-  setPreference();
-}
-
-
-
-
-  ThemeData dark = ThemeData(
-    useMaterial3: true,
-    outlinedButtonTheme: OutlinedButtonThemeData(
-      style: OutlinedButton.styleFrom(
-        foregroundColor: Colors.white,
-        side: BorderSide(color: Colors.white, width: 2),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-      ),
-    ),
-
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.amber,
-      brightness: Brightness.dark,
-    ),
-    appBarTheme: AppBarTheme(
-      backgroundColor: Colors.amber,
-      foregroundColor: Colors.black,
-    ),
-  );
-
-  ThemeData light = ThemeData(
-    outlinedButtonTheme: OutlinedButtonThemeData(
-      style: OutlinedButton.styleFrom(
-        foregroundColor: Colors.white,
-        side: BorderSide(color: Colors.white, width: 2),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-      ),
-    ),
-
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.amber,
-      brightness: Brightness.light,
-    ),
-    appBarTheme: AppBarTheme(
-      backgroundColor: Colors.amber,
-      foregroundColor: Colors.white,
-    ),
-  );
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: getPreference(),
-      builder:(context, _) => MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: isDark ? dark : light,
-      home: Scaffold(
-        appBar: AppBar(title: Text("Counter Apps"),
-        actions: [
-          IconButton(onPressed: refresh, icon: Icon(Icons.refresh))
-        ],)
-        ,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Angka saat ini adalah : $counter",
-                style: TextStyle(fontSize: 25),
-              ),
-              SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: minus, child: Icon(Icons.remove)),
-                  SizedBox(width: 20),
-                  ElevatedButton(onPressed: plus, child: Icon(Icons.add)),
-                ],
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: changeTheme,
-          child: Icon(Icons.color_lens),
-        ),
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: MaterialApp(
+        home: CounterPage(),
       ),
-    ),
     );
-}}
+  }
+}
+
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: BlocBuilder<CounterCubit, int>(
+        builder: (context, count) => Center(child: Text('$count')),
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () => context.watch<CounterCubit>().kali(),
+          ),
+          const SizedBox(height: 4),
+          FloatingActionButton(
+            child: const Icon(Icons.remove),
+            onPressed: () => context.read<CounterCubit>().decrement(),
+          ),
+        ],
+      ),
+    );
+  }
+}
