dc442dfb43b3c985c33dcd703bb9dcb9046528bc
hafidjs <afidzpratama@gmail.com>
2025-10-29 17:55:36 +0700

    animated container

--- lib/main.dart
+++ lib/main.dart
@@ -10,60 +10,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BlocProvider(create: (context) => CounterBloc(), child: HomePage()),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  bool click = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Center(
-        child: BlocConsumer<CounterBloc, int>(
-          buildWhen: (previous, current) {
-            // print("prev : $previous --- curr : $current");
-            if(current > 5) {
-              return true;
-            }
-            return false;
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              click = !click;
+            });
           },
-          listenWhen: (previous, current) {
-            print("curr : $current");
-            if(current > 10) {
-              return true;
-            }
-            return false;
-          },
-          listener: (context, state) {
-            if (state > 10) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text("Di atas 10 bro!"),
-                  duration: Duration(milliseconds: 300),
-                ),
-              );
-            }
-          },
-          builder: (context, state) {
-            return Text("Angka $state", style: TextStyle(fontSize: 35));
-          },
+          child: AnimatedContainer(
+            duration: Duration(seconds: 2),
+            width: click ? 100 : 300,
+            height: click ? 500 : 100,
+            color: Colors.amber,
+            curve: Curves.bounceInOut,
+            alignment: click ? Alignment.centerRight : Alignment.topCenter,
+            child: Icon(
+              Icons.apple,
+              size: 50,
+            ),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => context.read<CounterBloc>().increment(),
-        child: Icon(Icons.add),
       ),
     );
   }
-}
-
-class CounterBloc extends Cubit<int> {
-  CounterBloc() : super(0);
-
-  void increment() => emit(state + 1);
 }
