84ac4fda16fb3a2fd45829199425104e8bb49f3d
hafidjs <afidzpratama@gmail.com>
2025-11-05 15:41:50 +0700

    avatar glow

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 
-import 'package:get/get.dart';
-
-import 'app/routes/app_pages.dart';
-
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: AvatarGlow(
+  startDelay: const Duration(milliseconds: 200),
+  glowColor: Colors.white,
+  glowShape: BoxShape.circle,
+  curve: Curves.fastOutSlowIn,
+  child: const Material(
+    elevation: 8.0,
+    shape: CircleBorder(),
+    color: Colors.transparent,
+    child: CircleAvatar(
+      backgroundImage: AssetImage('assets/images/avatar.png'),
+      radius: 50.0,
     ),
-  );
+  ),
+),
+        ),
+      ),
+    );
+  }
 }
