df82a21a5c82978a78362235c5020ef9bd93ffce
hafidjs <afidzpratama@gmail.com>
2025-11-05 16:27:19 +0700

    Lottie

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,25 +10,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: AvatarGlow(
-  startDelay: const Duration(milliseconds: 200),
-  glowColor: Colors.white,
-  glowShape: BoxShape.circle,
-  curve: Curves.fastOutSlowIn,
-  child: const Material(
-    elevation: 8.0,
-    shape: CircleBorder(),
-    color: Colors.transparent,
-    child: CircleAvatar(
-      backgroundImage: AssetImage('assets/images/avatar.png'),
-      radius: 50.0,
-    ),
-  ),
-),
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(child: Lottie.asset("assets/lottie/password.json"),),
+              Text(
+                "Masukkan email kamu di sini, untuk mereset password kamu.",
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 40),
+              TextField(
+                autocorrect: false,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 20,
+                  ),
+                  labelText: "Email Address",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(onPressed: () {}, child: Text("RESET"), style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 20,
+                  ),
+                ),),
+              ),
+            ],
+          ),
         ),
       ),
     );
