933b638853fc6c4031489e18722f61cfeaddaf5b
hafidjs <afidzpratama@gmail.com>
2025-11-09 14:56:33 +0700

    ClipRRect

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,48 +19,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Container(child: Lottie.asset("assets/lottie/password.json"),),
-              Text(
-                "Masukkan email kamu di sini, untuk mereset password kamu.",
-                style: TextStyle(fontSize: 20),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 40),
-              TextField(
-                autocorrect: false,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 20,
-                  ),
-                  labelText: "Email Address",
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                width: double.infinity,
-                child: ElevatedButton(onPressed: () {}, child: Text("RESET"), style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50)
-                  ),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 20,
-                  ),
-                ),),
-              ),
-            ],
-          ),
+      appBar: AppBar(),
+      body: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(1500),
+        child: Container(
+          width: 250,
+          height: 500,
+          color: Colors.blue,
         ),
       ),
+      )
     );
   }
 }
