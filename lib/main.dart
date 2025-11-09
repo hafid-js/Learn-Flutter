295a0ff4585b4c7d791aa0677ea00d7642bc5bbc
hafidjs <afidzpratama@gmail.com>
2025-11-09 15:53:45 +0700

    arcToPoint

--- lib/main.dart
+++ lib/main.dart
@@ -33,15 +33,24 @@ class HomePage extends StatelessWidget {
 class MyClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-       var controlPoint1 = Offset(50, size.height - 100);
-    var controlPoint2 = Offset(size.width - 50, size.height);
-    var endPoint = Offset(size.width, size.height - 50);
-
+    double radius = 20;
     Path path = Path()
-      ..moveTo(size.width / 2, 0)
-      ..lineTo(0, size.height - 50)
-      ..cubicTo(controlPoint1.dx, controlPoint1.dy, controlPoint2.dx,
-          controlPoint2.dy, endPoint.dx, endPoint.dy)
+      ..moveTo(radius, 0)
+      ..lineTo(size.width - radius, 0)
+      ..arcToPoint(Offset(size.width, radius))
+      ..lineTo(size.width, size.height - radius)
+      ..arcToPoint(
+        Offset(size.width - radius, size.height),
+        radius: Radius.circular(radius),
+      )
+      ..lineTo(radius, size.height)
+      ..arcToPoint(
+        Offset(0, size.height - radius),
+        radius: Radius.circular(radius),
+        clockwise: false,
+      )
+      ..lineTo(0, radius)
+      ..arcToPoint(Offset(radius, 0), radius: Radius.elliptical(40, 20))
       ..close();
 
     return path;
