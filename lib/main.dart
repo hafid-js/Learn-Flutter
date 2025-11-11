76d70934c607564bbc9f4083926d7f3b726242ee
hafidjs <afidzpratama@gmail.com>
2025-11-11 19:23:14 +0700

    Sliver List

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:ui';
-
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,99 +5,65 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: CarouselWithIndicatorDemo());
+    return MaterialApp(home: HomePage());
   }
 }
 
-class CarouselWithIndicatorDemo extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _CarouselWithIndicatorState();
-  }
-}
-
-class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
-  int _current = 0;
-  final CarouselSliderController _controller = CarouselSliderController();
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Carousel with indicator controller demo')),
-      body: Column(children: [
-        Expanded(
-          child: CarouselSlider(
-            items: imageSliders,
-            carouselController: _controller,
-            options: CarouselOptions(
-                autoPlay: true,
-                enlargeCenterPage: true,
-                aspectRatio: 2.0,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _current = index;
-                  });
-                }),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.blue,
+            expandedHeight: 120,
+            floating: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("Silver List"),
+              background: FlutterLogo(),
+            ),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: imgList.asMap().entries.map((entry) {
-            return GestureDetector(
-              onTap: () => _controller.animateToPage(entry.key),
-              child: Container(
-                width: 12.0,
-                height: 12.0,
-                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: (Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black)
-                        .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-              ),
-            );
-          }).toList(),
-        ),
-      ]),
-    );
-  }
-}
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      builder: (context, value, g) {
-        return MaterialApp(
-          initialRoute: '/',
-          darkTheme: ThemeData.dark(),
-          themeMode: ThemeMode.values.toList()[value as int],
-          debugShowCheckedModeBanner: false,
-          routes: {
-            '/': (ctx) => CarouselDemoHome(),
-            '/basic': (ctx) => BasicDemo(),
-            '/nocenter': (ctx) => NoCenterDemo(),
-            '/image': (ctx) => ImageSliderDemo(),
-            '/complicated': (ctx) => ComplicatedImageDemo(),
-            '/enlarge': (ctx) => EnlargeStrategyDemo(),
-            '/manual': (ctx) => ManuallyControlledSlider(),
-            '/noloop': (ctx) => NoonLoopingDemo(),
-            '/vertical': (ctx) => VerticalSliderDemo(),
-            '/fullscreen': (ctx) => FullscreenSliderDemo(),
-            '/ondemand': (ctx) => OnDemandCarouselDemo(),
-            '/indicator': (ctx) => CarouselWithIndicatorDemo(),
-            '/prefetch': (ctx) => PrefetchImageDemo(),
-            '/reason': (ctx) => CarouselChangeReasonDemo(),
-            '/position': (ctx) => KeepPageviewPositionDemo(),
-            '/multiple': (ctx) => MultipleItemDemo(),
-            '/zoom': (ctx) => EnlargeStrategyZoomDemo(),
-          },
-        );
-      },
-      valueListenable: themeMode,
+
+          // SliverList(
+          //   delegate: SliverChildBuilderDelegate((ctx, index) {
+          //     return ListTile(
+          //       leading: CircleAvatar(),
+          //       title: Text("Data ke - ${index + 1}"),
+          //     );
+          //   }, childCount: 20),
+          // ),
+          // SliverList(delegate: SliverChildListDelegate.fixed(
+          //   [
+          //     Container(
+          //     height: 100,
+          //     color: Colors.green[600],
+          //   ),
+          //   Container(
+          //     height: 100,
+          //     color: Colors.pink[600],
+          //   )
+          //   ]
+          // ))
+
+          // SliverFixedExtentList(delegate: SliverChildBuilderDelegate(
+          //   (ctx, index) {
+          //     return Container(color: Colors.amber[100 * (index % 9)]);
+          //   }, childCount: 9
+          // ), itemExtent: 200)
+          SliverPrototypeExtentList(
+            delegate: SliverChildBuilderDelegate((ctx, index) {
+              return Container(color: Colors.amber[900]);
+            }, childCount: 1),
+            prototypeItem: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text("Halo"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
