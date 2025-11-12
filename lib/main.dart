cea8d0e34c1eec6e12a3010c3ac22b2b6b08d351
hafidjs <afidzpratama@gmail.com>
2025-11-12 16:17:47 +0700

    Sliver Grid

--- lib/main.dart
+++ lib/main.dart
@@ -19,74 +19,31 @@ class HomePage extends StatelessWidget {
         slivers: [
           SliverAppBar(
             pinned: true,
-            expandedHeight: 150,
+            expandedHeight: 125,
             // collapsedHeight: 100,
             flexibleSpace: FlexibleSpaceBar(
               background: FlutterLogo(),
-              title: Text("Title AppBar"),
+              title: Text("Sliver Grid"),
             ),
           ),
-          SliverPersistentHeader(pinned: true,delegate: MyClass(),),
-          SliverList(
+
+          SliverGrid(
             delegate: SliverChildBuilderDelegate((context, index) {
-              return Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Center(
-                    child: Text(
-                      "Item - ${index + 1}",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              );
-            }, childCount: 10),
+              return Container(color: Colors.amber[100 * (index % 9)]);
+            }, childCount: 50),
+            // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //   crossAxisCount: 3,
+            //   childAspectRatio: 12 / 7,
+            //   crossAxisSpacing: 10,
+            //   mainAxisSpacing: 10
+            // ),
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 100,
+              crossAxisSpacing: 10
+            ),
           ),
         ],
       ),
     );
   }
-}
-
-class MyClass extends SliverPersistentHeaderDelegate {
-@override
-Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-  return Stack(
-    fit: StackFit.expand,
-    children: [
-      Image.network(
-        "https://picsum.photos/id/100/1000/500",
-        fit: BoxFit.cover,
-        errorBuilder: (context, error, stackTrace) =>
-            Container(color: Colors.grey), // fallback
-      ),
-      Container(
-        alignment: Alignment.center,
-        color: Colors.black.withOpacity(0.3),
-        child: Text(
-          "Header Tetap",
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-      ),
-    ],
-  );
-}
-
-
-  @override
-  // TODO: implement maxExtent
-  double get maxExtent => 200;
-
-  @override
-  // TODO: implement minExtent
-  double get minExtent => 100;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-return false;
-  }
-  
 }
